8ae5d9fbb43b61ce9aa98e12506b594ad6d00da8
Giuseppe <gius4110@gmail.com>
2026-09-14 14:05:37 -0600

    Initial Commit

---
+++ IsePpeScanner/IsePpeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsePpeScannerApp.swift
+//  IsePpeScanner
+//
+//  Created by Giuseppe Lara Valdés on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct IsePpeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
